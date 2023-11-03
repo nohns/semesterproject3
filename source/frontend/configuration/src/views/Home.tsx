67c0aca102630e67edca5156a1c28a6d1e2a7e88
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-11-03 15:18:14 +0100

    feat: small introduction to stuff

--- source/frontend/configuration/src/views/Home.tsx
+++ source/frontend/configuration/src/views/Home.tsx
@@ -1,10 +1,51 @@
 /** @format */
 
+import useGetContainers from "@/api/endpoints/container/getContainers";
+import useGetDrinks from "@/api/endpoints/drinks/getDrinks";
+import useGetFluids from "@/api/endpoints/fluid/getFluids";
+import useGetImages from "@/api/endpoints/images/getImages";
+
 function Home(): JSX.Element {
+  //De her 4 er hooks som der laver API kald
+  const images = useGetImages();
+  //I kan kalde flere ting på dem de eneste som rigtigt er relevante er isLoading, isError og data
+  //Altså om den er igang med at loade dataen stadig, om der er sket en fejl og så selve dataen
+
+  //Nede I JSXen kan I så bruge de forskellige ting ofte bruger man conditional logik altså ternary operators(selvom det er noget pis) og så &&
+  //Når man har et array så bruger man en funktion som hedder map til at loope henover dataen så kan man nemlig få flere ting til at render
+  //Se neget i JSX'en
+
+  const fluids = useGetFluids();
+
+  const drinks = useGetDrinks();
+
+  const containers = useGetContainers();
+
+  //Her tjekker vi altså for om dataen er loadet ind endnu, hvis den ikke er så viser vi bare en loading besked
+  if (
+    images.isLoading ||
+    fluids.isLoading ||
+    drinks.isLoading ||
+    containers.isLoading
+  ) {
+    return <div>Loading...</div>;
+  }
+
+  //Hvis i åbner browseren og trykker inspect så kan i åbne developer tooling hvor i kan finde consollen browseren der logger jeg dataen ud
+  console.log(images.data?.images);
+  console.log(fluids.data?.fluids);
+  console.log(drinks.data?.drinks);
+  console.log(containers.data?.containers);
+
+  //Her kan i se at jeg bruger .map på data.images som jo er et array så kan vi render 3 billeder slef
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      {images.data?.images?.map((image) => (
+        <div key={image.id}>
+          <img src={image.path} />
+        </div>
+      ))}
+    </>
   );
 }
 
