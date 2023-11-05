95fc04fe6e403601293086a269611adc955f3ffc
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-11-05 15:09:49 +0100

    feat: hardcoded possible return values

--- source/embedded/controller/containers.py
+++ source/embedded/controller/containers.py
@@ -1,9 +1,8 @@
 from database.database import Database
 from domain.domain import FluidContainer
 
-
-def get_containers(database: Database):
+def get_containers(database: Database)-> list(FluidContainer):
     return 0
     
-def change_containers(database: Database, id: int):
+def change_containers(database: Database, id: int)->None:
     return 0;
