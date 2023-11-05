3f8e8599411145ec54a90175b9061e36bb585781
SofieSchou <sofie.schou@live.dk>
2023-11-05 14:57:13 +0100

    changes to controller class and its functions

--- source/embedded/controller/drinks.py
+++ source/embedded/controller/drinks.py
@@ -1,15 +1,27 @@
 from database.database import Database
 from domain.domain import Drink
 
-def create_drink(database: Database)->None:
+def create_drink(database: Database):
+
     return 0
     
-def delete_drink(database: Database, id: int,)->None:
-    return 0
+def delete_drink(id: int, database: Database):
+    try:
+        database.delete_drink(id)
+        return 1
+    except Exception as e:
+        print(f"error {e}, when trying to delete drink via controller")
+        return None
     
-def get_drinks(database: Database) -> list(Drink) :
-    return 0
+def get_drinks(database: Database):
+    try:
+        drinks=database.get_drinks()
+        return drinks
+    except Exception as e:
+      print(f"error {e}, when requesting drinks from database to controller")
+      return None
     
-def pour_drink(database: Database)->None:
+def pour_drink(database: Database):
+
     return 0
     
