f1bd4c421b07dc41d5be27b0184c36127ac1fa45
jrysz <jonas.rysz@gmail.com>
2023-11-06 02:58:34 +0100

    constructor not needed

--- source/embedded/domain/domain.py
+++ source/embedded/domain/domain.py
@@ -4,21 +4,12 @@ from dataclasses import dataclass
 # Domain classes
 @dataclass
 class Fluid:
-    def __init__(self, name: str):
-        self.name = name
-        self.id = None
-
     id: int
     name: str
 
 
 @dataclass
 class Ingredient:
-    def __init__(self, fluid: Fluid, amount_in_cl: int):
-        self.fluid = fluid
-        self.amount_in_cl = amount_in_cl
-        self.id = None
-
     id: int
     amount_in_cl: int
     fluid: Fluid
@@ -34,25 +25,12 @@ class Statistics:
 
 @dataclass
 class Image:
-    def __init__(self, path: str):
-        self.path = path
-        self.id = None
-
     id: int
     path: str
 
 
 @dataclass
 class Drink:
-    def __init__(
-        self, name: str, amount_in_cl: int, image: Image, ingredients: list[Ingredient]
-    ):
-        self.name = name
-        self.amount_in_cl = sum(ingredient.amount_in_cl for ingredient in ingredients)
-        self.image = image
-        self.ingredients = ingredients
-        self.id = None
-
     id: int
     name: str
     amount_in_cl: int  # 0-100
@@ -62,11 +40,6 @@ class Drink:
 
 @dataclass
 class FluidContainer:
-    def __init__(self, fluid_amount_in_cl: int, fluid_type: Fluid):
-        self.fluid_amount_in_cl = fluid_amount_in_cl
-        self.fluid_type = fluid_type
-        self.container_id = None
-
     container_id: int
     fluid_amount_in_cl: int
     fluid_type: Fluid
