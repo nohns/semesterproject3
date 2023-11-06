e96064e048df09f1b1fc55691f60ddd314209ad5
jrysz <jonas.rysz@gmail.com>
2023-11-06 01:23:08 +0100

    parameters added to reflect those in the api

--- source/embedded/controller/controller.py
+++ source/embedded/controller/controller.py
@@ -1,15 +1,18 @@
-#Call controller class
+# Call controller class
 from database.database import Database
 from netlink.publisher import NetlinkPublisher
+from domain.domain import *
 
 
-#<- IMPORT FUNCTIONS FROM OTHER FILES ->
+# <- IMPORT FUNCTIONS FROM OTHER FILES ->
 from controller.update import update
 from controller.containers import change_containers, get_containers
 from controller.drinks import create_drink, delete_drink, get_drinks, pour_drink
 from controller.fluids import get_fluids, create_fluid, delete_fluid
 from controller.images import get_images
-#<- IMPORT FUNCTIONS FROM OTHER FILES ->
+
+# <- IMPORT FUNCTIONS FROM OTHER FILES ->
+
 
 class Controller:
     database: Database
@@ -19,43 +22,35 @@ class Controller:
         self.database = database
         self.netlink_publisher = netlink_publisher
 
-    def change_containers(self, id: int):
+    def change_containers(self, id: int, fluid_type: str, fluid_amount_in_cl: int):
         return change_containers(id, self.database)
-    
+
     def get_containers(self):
         return get_containers(self.database)
-    
+
     def create_drink(self):
         return create_drink(self.database)
-    
-    def delete_drink(self):
-        return delete_drink(self.database)
-    
+
+    def delete_drink(self, id: int):
+        return delete_drink(id, self.database)
+
     def get_drinks(self):
         return get_drinks(self.database)
-    
+
     def pour_drink(self):
         return pour_drink(self.database)
-    
+
     def get_fluids(self):
         return get_fluids(self.database)
-    
-    def create_fluid(self):
+
+    def create_fluid(self, fluid: Fluid):
         return create_fluid(self.database)
-    
-    def delete_fluid(self):
+
+    def delete_fluid(self, id: int):
         return delete_fluid(self.database)
-    
+
     def get_images(self):
         return get_images(self.database)
 
     def update(self):
         return update(self.database)
-    
-
-
-
-
-    
-    
-        
