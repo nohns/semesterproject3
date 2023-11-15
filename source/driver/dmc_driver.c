1f85fe7cbdbe2d142569dbd3ae0fd7a74df8bfe9
Asger Nohns <asgernohns@gmail.com>
2023-11-15 21:25:19 +0000

    refactor(hw): move module main to separate file

--- source/driver/dmc_driver.c
+++
@@ -1,8 +0,0 @@
-#include "../common/hw/include/packet.h"
-#include "dmc/uart.h"
-
-int main()
-{
-  struct dmc_packet *packet =
-      (struct dmc_packet *)kmalloc(sizeof(struct dmc_packet), GFP_KERNEL);
-}
