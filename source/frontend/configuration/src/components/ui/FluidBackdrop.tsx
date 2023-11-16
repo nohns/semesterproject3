61747199b4ca1609c771e45ed914de1eacf03168
CDHush <hassan.ali231102@gmail.com>
2023-11-16 10:44:07 +0100

    Update?
    update?

--- source/frontend/configuration/src/components/ui/FluidBackdrop.tsx
+++ source/frontend/configuration/src/components/ui/FluidBackdrop.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import CustomDropdown from "@/components/Dropdown";
-import ShowImage from "@/views/image/ShowImage";
-import useGetImages, { Image } from "@/api/endpoints/images/getImages";
-import Overblik_80-100.png from "@/assets/Overblik_80-100.png";
-const images = useGetImages();
+// import ShowImage from "@/views/image/ShowImage";
+// import useGetImages, { Image } from "@/api/endpoints/images/getImages";
+// import Overblik_80_100 from "@/assets/Overblik_80_100.png";
+// const images = useGetImages();
 
 const FluidBackdrop: React.FC = () => {
  
@@ -12,7 +12,7 @@ const FluidBackdrop: React.FC = () => {
     <div className="bg-neutral-400 rounded-2xl w-56 aspect-square flex flex-col items-center gap-5 p-2 transition duration-300 hover:bg-neutral-500">
       <CustomDropdown className="bg-slate-100 w-40" />
       <div className="w-[40%] h-[60%] bg-white">
-        <ShowImage images={images} />
+        {/* <ShowImage image={images} /> */}
         {/* Add your additional content here if needed */}
       </div>
     </div>
