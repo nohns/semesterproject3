7d14e3cfcc9d3f7b89495ead5c24747a46992049
Asger Nohns <asgernohns@gmail.com>
2023-11-16 13:26:38 +0000

    feat(hw): complete prototype for uart handler

--- source/driver/dmc/uart/handler.h
+++ source/driver/dmc/uart/handler.h
@@ -3,6 +3,8 @@
 
 #include <linux/types.h>
 
+#include "packet.h"
+
 #include "../util.h"
 
 struct dmc_uart_handler
@@ -31,12 +33,12 @@ struct dmc_uart_handler
 /**
  * @brief Registers the necessary resources for the uart to function
  */
-int dmc_uart_register(struct dmc_uart_handler *uart);
+int dmc_uart_handler_register(struct dmc_uart_handler *uart);
 
 /**
  * @brief Unregisters uart resources
  */
-int dmc_uart_unregister(struct dmc_uart_handler *uart);
+int dmc_uart_handler_unregister(struct dmc_uart_handler *uart);
 
 /**
  * @brief Starts the receiving sequence for a reading byte from the RX gpio,
@@ -44,6 +46,12 @@ int dmc_uart_unregister(struct dmc_uart_handler *uart);
  * be called when start bit is receiving, which in general should be when the
  * interrupt triggers
  */
-int dmc_uart_start_recv(struct dmc_uart_handler *uart);
+int dmc_uart_handler_start_recv(struct dmc_uart_handler *uart);
+
+/**
+ * @brief Sends a packet over the uart
+ */
+int dmc_uart_handler_send_packet(struct dmc_uart_handler *uart,
+                                 struct dmc_packet       *packet);
 
 #endif // DMC_UART_HANDLER_H
