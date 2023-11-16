09432f38c5dc08d9b548812a2ae83e12150fc499
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-11-16 22:28:01 +0100

    Feat: Add state to containers

--- source/frontend/configuration/src/views/Home.tsx
+++ source/frontend/configuration/src/views/Home.tsx
@@ -23,57 +23,40 @@ import Footer from "@/components/Footer";
 
 import CustomDropdown from "@/components/Dropdown";
 import FluidBackdrop from "@/components/ui/FluidBackdrop";
+import { useEffect, useState } from "react";
+import { Drink } from "@/api/endpoints/drinks/getDrinks";
 
 function Home(): JSX.Element {
   const fluids = useGetFluids();
 
   const calc = useCalculateDrinks();
-  let possibleDrinks = calc?.calculate(fluids.data?.fluids!);
+  //Jeg mangler stadig at gøre default staten til whatever som vi læser ind fra fluid containersne
+  const [possibleDrinks, setPossibleDrinks] = useState<Drink[]>();
 
-  //const drinks = useGetDrinks();
+  //let possibleDrinks = calc?.calculate(fluids.data?.fluids!);
 
-  //const containers = useGetContainers();
-
-  //Lige noget test data for at validere magien
-  /*   const fluids: Fluid[] = [
-    {
-      id: 4,
-      name: "Vodka",
-    },
-    {
-      id: 5,
-      name: "Rom",
-    },
-    {
-      id: 2,
-      name: "Blå booster",
-    },
-    {
-      id: 3,
-      name: "Vand",
-    },
-    {
-      id: 1,
-      name: "Coca Cola",
-    },
-  ]; */
-
-  //Den her funktion returnerer et object calc som udelukkende returnerer en funktion som hedder calculate som kan bruges til at beregne drinks kombinationer
-  //Så her kan man jo så kalde calculate med et array af fluids, det er så meningen at jeres dropdown menuer skal tilføje noget state som danner et array af fluid arrays :)
-  //Umiddelbart ville jeg bruge en useEffect hook hvor dependency arrayet er et array af jeres state arrays så kan i bare kalde calculate i useEffecten
-
-  console.log(possibleDrinks);
-  //Så ville jeg lave et komponent som tager imod en drink og displayer den
-
-  const [container1, setContainer1] = useState<Fluid>();
-  const [container2, setContainer2] = useState<Fluid>();
-  const [container3, setContainer3] = useState<Fluid>();
+  const [container1, setContainer1] = useState<Fluid>({
+    id: 0,
+    name: "Vælg væske",
+  });
+  const [container2, setContainer2] = useState<Fluid>({
+    id: 0,
+    name: "Vælg væske",
+  });
+  const [container3, setContainer3] = useState<Fluid>({
+    id: 0,
+    name: "Vælg væske",
+  });
 
   //Den her react funktion bliver kaldt hver gang at container1, container2 eller container3 ændrer sig dvs hvis i ændrer jeres dropdown menuer så vil den her funktion blive kaldt
   //Og så genberegner den mulige drinks
   useEffect(() => {
     console.log("Beregner en ny combination af drinks");
-    possibleDrinks = calc?.calculate(fluids.data?.fluids!);
+    const combinations = [container1, container2, container3];
+    setPossibleDrinks(calc?.calculate(combinations));
+
+    //possibleDrinks = calc?.calculate(combinations);
+    console.log(possibleDrinks);
   }, [container1, container2, container3]);
 
   //Hvis der ikke er nogen fluids så kan vi ikke loade UI'et så vi returnerer bare en loading tekst
@@ -88,9 +71,21 @@ function Home(): JSX.Element {
         <Header />
         <div className="flex flex-col items-center mt-[-50px]">
           <div className="flex flex-row justify-center space-x-7 mt-10">
-            <FluidBackdrop />
-            <FluidBackdrop />
-            <FluidBackdrop />
+            <FluidBackdrop
+              container={container1!}
+              setContainer={setContainer1}
+              fluids={fluids.data?.fluids!}
+            />
+            <FluidBackdrop
+              container={container2!}
+              setContainer={setContainer2}
+              fluids={fluids.data?.fluids!}
+            />
+            <FluidBackdrop
+              container={container3!}
+              setContainer={setContainer3}
+              fluids={fluids.data?.fluids!}
+            />
           </div>
           <div className="bg-emerald-600 mt-5 py-12  px-80 rounded-2xl transition duration-300 hover:bg-emerald-600 hover:darken-2 hover:opacity-75">
             Possible drink possibilities
