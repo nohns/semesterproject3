7db174ecef0051181a66eef149e0b06d25ab77c9
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-11-17 12:24:33 +0100

    feat: no tested container api func

---
+++ source/frontend/touchscreen/src/api/endpoints/container/container.ts
@@ -0,0 +1,46 @@
+/** @format */
+
+import ENV from "@/constants/env";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { http } from "@/api/axios";
+import { Fluid } from "../fluid/getFluids.ts";
+
+if (ENV.MOCKED) {
+  await import("./getContainers.mock.ts");
+}
+
+export interface FluidContainer {
+  id: number;
+  fluid: Fluid;
+  fluidAmountInCl: number;
+}
+
+interface GetContainersRequest {}
+
+export interface GetContainersResponse {
+  containers: FluidContainer[];
+}
+
+const getContainers = async ({}: GetContainersRequest) => {
+  const response = await http.get<GetContainersResponse>(`v1/containers`);
+
+  if (response.status !== 200) {
+    console.log(response);
+    throw new Error("No data");
+  }
+
+  return response.data;
+};
+
+const useGetContainers = () => {
+  const query = useQuery({
+    queryKey: ["getContainers"],
+    queryFn: () => getContainers({}),
+  });
+
+  return query;
+};
+
+export default useGetContainers;
