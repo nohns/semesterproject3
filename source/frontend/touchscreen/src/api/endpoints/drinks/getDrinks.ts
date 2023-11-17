be660ff5b630e3f76a9125a48e041ccfdf4df47d
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-11-17 12:24:47 +0100

    feat: aditional interfaces for dirnk

--- source/frontend/touchscreen/src/api/endpoints/drinks/getDrinks.ts
+++ source/frontend/touchscreen/src/api/endpoints/drinks/getDrinks.ts
@@ -14,13 +14,24 @@ export interface Drink {
   id: number;
   image: Image;
   name: string;
+  remainingFluid?: number;
+  ingredients: Ingredient[];
+}
+
+export interface Ingredient {
+  id: number;
+  fluid: Fluid;
   amountInCl: number;
-  remainingFluid: number;
 }
 
 interface Image {
   id: number;
   path: string;
+}
+
+interface Fluid {
+  id: number;
+  name: string;
 }
 
 interface GetDrinksRequest {}
