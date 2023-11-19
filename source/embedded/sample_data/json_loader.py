7e31f8a2e47620d3759ccdddafd7110773beab0d
jrysz <jonas.rysz@gmail.com>
2023-11-19 21:24:02 +0100

    added module that loads json drinks and fluids

---
+++ source/embedded/sample_data/json_loader.py
@@ -0,0 +1,13 @@
+import json
+from domain.domain import json_to_dataclass
+
+
+def load_json_file(file_path, dataclass_type):
+    with open(file_path, "r") as file:
+        json_data = json.load(file)
+
+    instances = []
+    for item in json_data:
+        instances.append(json_to_dataclass(item, dataclass_type))
+
+    return instances
