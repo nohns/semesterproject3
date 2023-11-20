5a4d7ee15a2c03bbee614a4049b0217766033c01
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-11-20 13:26:36 +0100

    feat: use calc hooks

--- source/frontend/touchscreen/src/views/Selection.tsx
+++ source/frontend/touchscreen/src/views/Selection.tsx
@@ -1,14 +1,14 @@
 /** @format */
 
-import DrinkSelection from "@/components/DrinkSelection";
+import DrinkCard from "@/components/DrinkCard";
 import { DrinkMachineState } from "@/App";
-import { useState } from "react";
 import Spinner from "@/components/Spinner";
 import { Button } from "@/components/ui/button";
-import { Toaster } from "@/components/ui/toaster";
 
 import usePourDrink from "@/api/endpoints/drinks/pourDrink";
-import useGetDrinks, { Drink } from "@/api/endpoints/drinks/getDrinks";
+import { Drink } from "@/api/endpoints/drinks/getDrinks";
+import useCalculateDrinks from "@/hooks/useCalculateDrinks";
+import { Toaster } from "@/components/ui/toaster";
 
 interface SelectionProps {
   setView: React.Dispatch<React.SetStateAction<DrinkMachineState>>;
@@ -21,29 +21,24 @@ function Selection({
   selectedDrink,
   setSelectedDrink,
 }: SelectionProps): JSX.Element {
-  const drinksQuery = useGetDrinks();
+  const calc = useCalculateDrinks();
+  const drinks = calc.calcPossibleDrinks();
+  console.log("Current configured drinks", drinks);
 
-  if (drinksQuery.error) {
-    return <div>{`:()`}</div>;
-  }
-
-  //Create some state to manage which drink has been selected
-  //const [selectedDrink, setSelectedDrink] = useState<number>();
-
-  const mutate = usePourDrink();
+  const pour = usePourDrink();
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
 
-    mutate.mutate({ id: selectedDrink?.id! });
+    pour.mutate({ id: selectedDrink?.id! });
 
     setView(DrinkMachineState.Pouring);
   };
 
-  if (drinksQuery.isLoading) {
+  if (!drinks) {
     return (
       <>
-        <div className="flex flex-col items-center h-full w-full  ">
+        <div className="flex flex-col items-center h-full w-full">
           <h1 className="scroll-m-20  text-white mt-10 text-6xl font-extrabold  tracking-tight lg:text-5xl border-b border-white">
             SELECT DRINK
           </h1>
@@ -51,8 +46,9 @@ function Selection({
           <Button
             onClick={() => setView(DrinkMachineState.Pouring)}
             className="mt-auto mb-8 p-8 bg-white"
+            disabled
           >
-            <h2 className="scroll-m-20  text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+            <h2 className="scroll-m-20 w-72  text-3xl font-semibold tracking-tight transition-colors first:mt-0">
               Pour drink
             </h2>
           </Button>
@@ -63,15 +59,15 @@ function Selection({
 
   return (
     <>
-      <div className="flex flex-col items-center h-full w-full   ">
+      <div className="flex flex-col items-center h-full w-full">
         <div className="drop-shadow-2xl shadow-green-700 mt-10 pb-4">
           <h1 className="scroll-m-20 text-white   text-6xl font-extrabold tracking-tight lg:text-5xl   ">
             SELECT DRINK
           </h1>
         </div>
 
-        {drinksQuery.data?.drinks?.map((drink) => (
-          <DrinkSelection
+        {drinks?.map((drink) => (
+          <DrinkCard
             key={drink.id}
             drink={drink}
             selectedDrink={selectedDrink!}
@@ -84,12 +80,11 @@ function Selection({
           className="mt-auto mb-8 p-8 bg-white"
           disabled={selectedDrink ? false : true}
         >
-          <h2 className="scroll-m-20  text-3xl font-semibold tracking-tight transition-colors first:mt-0 ">
+          <h2 className="scroll-m-20 w-72 text-3xl font-semibold tracking-tight transition-colors first:mt-0 ">
             Pour Drink
           </h2>
         </Button>
       </div>
-      <Toaster />
     </>
   );
 }
