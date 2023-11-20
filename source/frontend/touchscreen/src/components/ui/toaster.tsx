6fc9bb3171f032a4aadb0d84a0cc8d2a416c6109
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-11-20 13:25:58 +0100

    feat: small ui changes to toaster

--- source/frontend/touchscreen/src/components/ui/toaster.tsx
+++ source/frontend/touchscreen/src/components/ui/toaster.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import {
   Toast,
   ToastClose,
@@ -5,29 +7,31 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts } = useToast();
 
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
+            <div className="grid gap-1 border-b">
+              {title && <ToastTitle className="text-xl ">{title}</ToastTitle>}
               {description && (
-                <ToastDescription>{description}</ToastDescription>
+                <ToastDescription className="text-lg">
+                  {description}
+                </ToastDescription>
               )}
             </div>
             {action}
             <ToastClose />
           </Toast>
-        )
+        );
       })}
       <ToastViewport />
     </ToastProvider>
-  )
+  );
 }
