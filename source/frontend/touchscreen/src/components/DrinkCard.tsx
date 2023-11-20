856d3df8c9795d7be11974d1c186142bbe2e103b
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-11-20 13:25:45 +0100

    rename: superior name

--- source/frontend/touchscreen/src/components/DrinkCard.tsx
+++ source/frontend/touchscreen/src/components/DrinkCard.tsx
@@ -3,19 +3,20 @@ import { Drink, Ingredient } from "@/api/endpoints/drinks/getDrinks";
 
 import { Card, CardTitle } from "./ui/card";
 import { useToast } from "@/components/ui/use-toast";
+import useCalculateDrinks from "@/hooks/useCalculateDrinks";
 import { Toaster } from "./ui/toaster";
 
-interface DrinkSelectionProps {
+interface DrinkCardProps {
   drink: Drink;
   selectedDrink: Drink | undefined;
   setSelectedDrink: React.Dispatch<React.SetStateAction<Drink | undefined>>;
 }
 
-function DrinkSelection({
+function DrinkCard({
   drink,
   selectedDrink,
   setSelectedDrink,
-}: DrinkSelectionProps): JSX.Element {
+}: DrinkCardProps): JSX.Element {
   const handleSelectDrink = () => {
     setSelectedDrink(drink);
     beat2.play();
@@ -23,8 +24,7 @@ function DrinkSelection({
 
   //I need a function which calculates the total amountInCl of the drink based on the array of ingredients
 
-  const indicatorHeight = `${drink.remainingFluid}%`;
-  console.log(indicatorHeight);
+  //const indicatorHeight = `${drink.remainingFluid}%`;
 
   let beat = new Audio("src/assets/noselect.mp3");
   beat.volume = 0.1;
@@ -32,13 +32,18 @@ function DrinkSelection({
   let beat2 = new Audio("src/assets/jaja.mp3");
   beat2.volume = 0.1;
 
+  const calc = useCalculateDrinks();
+
+  const pourable = calc.calcFluidLeft(drink.ingredients);
+  const fluidLeft = pourable?.lowestFluidLeft;
+
   const { toast } = useToast();
 
   return (
     <>
       <Card
-        onClick={drink.remainingFluid <= 10 ? undefined : handleSelectDrink}
-        className={`flex flex-col w-80 h-60 m-3 bg-zinc-700 shadow-slate-800 shadow-2xl border-2 border-zinc-600 ${
+        onClick={!pourable.drinkPourable ? undefined : handleSelectDrink}
+        className={`flex flex-col w-80 h-60 m-3 bg-zinc-800 shadow-slate-800 shadow-2xl border-2 border-zinc-600 ${
           selectedDrink === drink ? "bg-zinc-800 selectedCard" : ""
         }`}
         style={{
@@ -49,15 +54,14 @@ function DrinkSelection({
           position: "relative", // Add position relative to the card
         }}
       >
-        {drink.remainingFluid <= 10 && (
+        {!pourable.drinkPourable && (
           <div
-            className="absolute top-0 left-0 w-full h-full flex items-center text-9xl justify-center text-red-500  text-center"
+            className="absolute top-0 left-0 w-full h-full flex items-center text-[10rem] justify-center text-red-700  text-center"
             /*  style={{ pointerEvents: "none" }} */
             onClick={() => {
               toast({
-                variant: "destructive",
-                title: `Oh oh! Refill container with ${drink.name} `,
-                description: "Error: Refill container with vodka",
+                title: `Oh oh! Its maintenance time!`,
+                description: `Refill container ${pourable.fluidThatMustBeRefilled[0].id} with ${pourable.fluidThatMustBeRefilled[0].fluid.name} to get this drink back on the menu!`,
               });
             }}
           >
@@ -66,31 +70,18 @@ function DrinkSelection({
         )}
         <CardTitle className="text-white mx-auto pt-2">{drink.name}</CardTitle>
 
-        <div className="flex flex-row justify-between mx-1 ">
-          <div>
-            <div className="relative h-4">
-              <div
-                className="absolute bottom-0 left-0 w-full bg-blue-500"
-                style={{ height: indicatorHeight }}
-              ></div>
-            </div>
-          </div>
+        {/*   {pourable.drinkPourable && ( */}
+        <div
+          className={`h-8 rounded-b-xl mt-auto text-center text-lg  border-white ${
+            pourable.drinkPourable ? "bg-green-400" : "bg-red-400"
+          }`}
+        >
+          {fluidLeft} cl remaining
         </div>
-
-        {drink.remainingFluid <= 10 && (
-          <div className="h-8 rounded-b-xl mt-auto text-center text-lg bg-red-400 border-white">
-            Refill container with vodka
-          </div>
-        )}
-
-        {drink.remainingFluid > 10 && (
-          <div className="h-8 rounded-b-xl mt-auto text-center text-lg bg-green-400 border-white">
-            {drink.remainingFluid} % remaining
-          </div>
-        )}
+        {/*   )} */}
       </Card>
     </>
   );
 }
 
-export default DrinkSelection;
+export default DrinkCard;
