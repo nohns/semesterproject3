119b97eae0cf6bd6e9973f32f5b73385238a2af9
Asger Nohns <asgernohns@gmail.com>
2023-11-22 09:34:23 +0000

    feat(hw): added debug print macro on rpi

--- source/driver/dmc/util.h
+++ source/driver/dmc/util.h
@@ -1,11 +1,20 @@
 #ifndef DMC_UTIL_H
 #define DMC_UTIL_H
 
+#include "../dmc_driver.h"
+
 // Adapted from: init_exit_cdev.c (see SOURCES in the top of this file)
 #define ERRGOTO(label, ...)                                                    \
   {                                                                            \
     pr_err(__VA_ARGS__);                                                       \
     goto label;                                                                \
+  }                                                                            \
+  while (0)
+
+// Adapted from: init_exit_cdev.c (see SOURCES in the top of this file)
+#define DMC_D(...)                                                             \
+  {                                                                            \
+    pr_debug(__VA_ARGS__);                                                     \
   }                                                                            \
   while (0)
 
