9ec6d7e66dfabb4fd78a0b402d8ab33dc303988f
Asger Nohns <asgernohns@gmail.com>
2023-11-22 21:12:04 +0000

    feat(driver): add base event netlink to py ex

--- source/driver/test_client.py
+++ source/driver/test_client.py
@@ -8,23 +8,41 @@ DMC_DRIVER_GENL_VERSION = 1
 DMC_EVENT_TYPE_GENL_USER_CONFIRM = 3
 DMC_EVENT_TYPE_GENL_FLUID_POUR_REQUESTED = 4
 
-class UserConfirmEvent(genlmsg):
-    cmd = DMC_EVENT_TYPE_GENL_USER_CONFIRM
+class BaseEvent(genlmsg):
     version = DMC_DRIVER_GENL_VERSION
+    
+    def __init__(self, *argv, **kwarg):
+        super().__init__(*argv, **kwarg)
+        self['cmd'] = self.evt_type
+        self['version'] = DMC_DRIVER_GENL_VERSION
+
+class UserConfirmEvent(BaseEvent):
+    evt_type = DMC_EVENT_TYPE_GENL_USER_CONFIRM
     nla_map = (
         ('DMC_EVENT_GENL_USER_CONFIRM_ATTR_UNSPECIFIED', 'none'),
         ('DMC_EVENT_GENL_USER_CONFIRM_ATTR_TYPE', 'uint8'),
     )
 
-class FluidPourRequestedEvent(genlmsg):
-    cmd = DMC_EVENT_TYPE_GENL_FLUID_POUR_REQUESTED
-    version = DMC_DRIVER_GENL_VERSION
+    def set(self):
+        self['attrs'] = [
+            ('DMC_EVENT_GENL_USER_CONFIRM_ATTR_TYPE', self.evt_type)
+        ]
+
+class FluidPourRequestedEvent(BaseEvent):
+    evt_type = DMC_EVENT_TYPE_GENL_FLUID_POUR_REQUESTED
     nla_map = (
         ('DMC_EVENT_GENL_FLUID_POUR_REQUESTED_ATTR_UNSPECIFIED', 'none'),
         ('DMC_EVENT_GENL_FLUID_POUR_REQUESTED_ATTR_TYPE', 'uint8'),
         ('DMC_EVENT_GENL_FLUID_POUR_REQUESTED_ATTR_CONTAINER', 'uint8'),
         ('DMC_EVENT_GENL_FLUID_POUR_REQUESTED_ATTR_AMOUNT', 'uint8'),
     )
+
+    def set(self, container, amount):
+        self['attrs'] = [
+            ('DMC_EVENT_GENL_FLUID_POUR_REQUESTED_ATTR_TYPE', self.evt_type),
+            ('DMC_EVENT_GENL_FLUID_POUR_REQUESTED_ATTR_CONTAINER', container),
+            ('DMC_EVENT_GENL_FLUID_POUR_REQUESTED_ATTR_AMOUNT', amount),
+        ]
 
 # Define the Netlink group ID you want to listen to
 NETLINK_GROUP_ID = 1
@@ -35,14 +53,7 @@ RLINK_CMD_REQ = 1
 def send_user_confirm_event(socket: GenericNetlinkSocket):
     # Create a new message
     msg = UserConfirmEvent()
-
-    msg['cmd'] = DMC_EVENT_TYPE_GENL_USER_CONFIRM
-    msg['version'] = DMC_DRIVER_GENL_VERSION
-
-    # Set the attributes
-    msg['attrs'] = [
-        ('DMC_EVENT_GENL_USER_CONFIRM_ATTR_TYPE', DMC_EVENT_TYPE_GENL_USER_CONFIRM)
-    ]
+    msg.set()
 
     # Send the message
     socket.nlm_request(msg, socket.prid, msg_flags=NLM_F_REQUEST)
@@ -50,16 +61,7 @@ def send_user_confirm_event(socket: GenericNetlinkSocket):
 def send_fluid_pour_requested_event(socket: GenericNetlinkSocket, container, amount):
     # Create a new message
     msg = FluidPourRequestedEvent()
-
-    msg['cmd'] = DMC_EVENT_TYPE_GENL_FLUID_POUR_REQUESTED
-    msg['version'] = DMC_DRIVER_GENL_VERSION
-
-    # Set the attributes
-    msg['attrs'] = [
-        ('DMC_EVENT_GENL_FLUID_POUR_REQUESTED_ATTR_TYPE', DMC_EVENT_TYPE_GENL_FLUID_POUR_REQUESTED),
-        ('DMC_EVENT_GENL_FLUID_POUR_REQUESTED_ATTR_CONTAINER', container),
-        ('DMC_EVENT_GENL_FLUID_POUR_REQUESTED_ATTR_AMOUNT', amount),
-    ]
+    msg.set(container, amount)
 
     # Send the message
     socket.nlm_request(msg, socket.prid, msg_flags=NLM_F_REQUEST)
