ac29006795e73642c76a7df347f666a1251a43ab
MadsSoeborg <madssoborg@gmail.com>
2023-11-23 15:29:35 +0100

    Killing NetLink so it works on windows, but you can't stop the server now

--- source/embedded/main.py
+++ source/embedded/main.py
@@ -11,13 +11,13 @@ from api.api import Api
 class Main:
     # Database and publishing events to psoc via netlink (pump control)
     database: Database
-    netlink_publisher: NetlinkPublisher
+    #netlink_publisher: NetlinkPublisher
 
     # Controller which handles logic will import domain classes
     controller: Controller
 
     # Recieving events from psoc via netlink (weight sensor)
-    netlink_reciever: NetlinkReciever
+    #netlink_reciever: NetlinkReciever
     # Recieving http requests from frontend
     api: Api
 
@@ -30,23 +30,23 @@ class Main:
 
         # Since we are going to be recieving events from psoc and http requests from frontend we need to create threads for both
         print("Setting up boundaries...")
-        netlink_reciever = NetlinkReciever(controller)
+        #netlink_reciever = NetlinkReciever(controller)
         api = Api(controller)
 
         # Create threads for netlink_reciever and api
-        netlink_reciever_thread = threading.Thread(target=netlink_reciever.run)
+        #netlink_reciever_thread = threading.Thread(target=netlink_reciever.run)
         api_thread = threading.Thread(target=api.run)
 
         # Start the threads
         print("Starting boundary threads...")
-        netlink_reciever_thread.start()
+        #netlink_reciever_thread.start()
         api_thread.start()
 
         # Wait for the threads to finish (which should be never)
-        netlink_reciever_thread.join()
+        #netlink_reciever_thread.join()
         api_thread.join()
         # api = Api(controller)
 
-
+ 
 if __name__ == "__main__":
     Main()
--- source/embedded/netlink/publisher.py
+++ source/embedded/netlink/publisher.py
@@ -1,35 +1,47 @@
-from pyroute2.netlink import NLM_F_REQUEST
-from pyroute2.netlink.generic import GenericNetlinkSocket
+import sys
+is_linux = sys.platform == "Linux"
+if is_linux:
+    from pyroute2.netlink import NLM_F_REQUEST
+    from pyroute2.netlink.generic import GenericNetlinkSocket
 
-from netlink.commands import UserConfirmEvent, FluidPourRequestedEvent
+    
+    from netlink.commands import UserConfirmEvent, FluidPourRequestedEvent
 
-# Configuration
-GENL_FAMILY = 'DMC_DRIVER_FAM'
+    # Configuration
+    GENL_FAMILY = 'DMC_DRIVER_FAM'
 
-# call netlink publisher class
-class NetlinkPublisher:
-    genlsock: GenericNetlinkSocket
+    # call netlink publisher class
+    class NetlinkPublisher:
+        genlsock: GenericNetlinkSocket
 
-    def __init__(self):
-        # Setup generic netlink socket for publishing events, by binding their usage to the generic netlink family
-        self.genlsock = GenericNetlinkSocket()
-        self.genlsock.bind(GENL_FAMILY, UserConfirmEvent)
-        self.genlsock.bind(GENL_FAMILY, FluidPourRequestedEvent)
-        
-        print("NetlinkPublisher initialized")
+        def __init__(self):
+            # Setup generic netlink socket for publishing events, by binding their usage to the generic netlink family
+            self.genlsock = GenericNetlinkSocket()
+            self.genlsock.bind(GENL_FAMILY, UserConfirmEvent)
+            self.genlsock.bind(GENL_FAMILY, FluidPourRequestedEvent)
+            
+            print("NetlinkPublisher initialized")
 
-    def user_confirm(self):
-        # Create a new message
-        msg = UserConfirmEvent()
-        msg.set()
+        def user_confirm(self):
+            # Create a new message
+            msg = UserConfirmEvent()
+            msg.set()
 
-        # Send the event message
-        self.genlsock.nlm_request(msg, self.genlsock.prid, msg_flags=NLM_F_REQUEST)
+            # Send the event message
+            self.genlsock.nlm_request(msg, self.genlsock.prid, msg_flags=NLM_F_REQUEST)
 
-    def pour_fluid(self, container: int, amount: int):
-        # Create a new message
-        msg = FluidPourRequestedEvent()
-        msg.set(container, amount)
+        def pour_fluid(self, container: int, amount: int):
+            # Create a new message
+            msg = FluidPourRequestedEvent()
+            msg.set(container, amount)
 
-        # Send the event message
-        self.genlsock.nlm_request(msg, self.genlsock.prid, msg_flags=NLM_F_REQUEST)
+            # Send the event message
+            self.genlsock.nlm_request(msg, self.genlsock.prid, msg_flags=NLM_F_REQUEST)
+
+else:
+    class NetlinkPublisher:
+        def __init__(self):
+            print("NetlinkPublisher disabled cause not linux")
+
+        def run(self):
+            print("NetlinkPublisher disabled cause not linux")
