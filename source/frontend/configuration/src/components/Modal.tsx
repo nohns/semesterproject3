fdc4388607ccac07af401177a0eb35b45e12015e
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-11-25 18:03:02 +0100

    feat: modal boilerplate

---
+++ source/frontend/configuration/src/components/Modal.tsx
@@ -0,0 +1,20 @@
+/** @format */
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import React from "react";
+
+interface ModalProps {}
+
+function Modal({}: ModalProps) {
+  return <div>content</div>;
+}
+
+export default Modal;
