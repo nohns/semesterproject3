9166c1447a8c05c506a83a3acbce0315bb2563bd
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-11-25 17:55:54 +0100

    bugfix: fix viewhight

--- source/frontend/configuration/src/App.tsx
+++ source/frontend/configuration/src/App.tsx
@@ -5,7 +5,7 @@ import Home from "./views/Home";
 function App() {
   return (
     <>
-      <div className="dark flex flex-col min-h-[120vh] w-screen bg-gradient-to-t from-[#302E37] to bg-slate-800">
+      <div className="dark flex flex-col h-screen w-screen bg-gradient-to-t from-[#302E37] to bg-slate-800">
         <Home />
       </div>
     </>
--- source/frontend/configuration/src/components/Footer.tsx
+++ source/frontend/configuration/src/components/Footer.tsx
@@ -4,16 +4,18 @@ import { GiDrinkMe } from "react-icons/gi";
 export default function Footer(): JSX.Element {
   return (
     <footer className="px-4 pb-2 w-screen mt-auto">
-      <div className="text-xl font-bold whitespace-nowrap text-white flex items-center justify-between pl-4 pr-4">
+      <div className="text-xl font-bold whitespace-nowrap text-white flex items-center justify-left pl-4 pr-4">
         <GiDrinkMe className="h-6 w-6 mr-2 text-white" />
         DRINKSMACHINE
       </div>
       <hr className="my-3 sm:mx-auto bg-red-500" />
-      <hr className="my-2 sm:mx-auto bg-red-500" />
+
       <span className="block text-s text-gray-400 sm:text-center mb-1">
-       <p><strong>Contact info:</strong></p>
-       <p>Email: contact@drinksmachine.com</p>
-       <p>Phone: (45) 11111111</p>
+        <p>
+          <strong>Contact info:</strong>
+        </p>
+        <p>Email: contact@drinksmachine.com</p>
+        <p>Phone: (45) 11111111</p>
       </span>
       <span className="block text-xs text-gray-400 sm:text-center ">
         © 2023 DRINKSMACHINE™
