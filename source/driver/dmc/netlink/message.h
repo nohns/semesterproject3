53905ac5da823e8d02faa083a4dba245686336d2
Asger Nohns <asgernohns@gmail.com>
2023-11-27 12:53:35 +0000

    feat(driver): add debug event

--- source/driver/dmc/netlink/message.h
+++ source/driver/dmc/netlink/message.h
@@ -32,6 +32,19 @@ int dmc_netlink_unmarshal_event_fluid_pour_requested(
     struct dmc_event_fluid_pour_requested *event,
     struct dmc_netlink_event_msg          *event_msg);
 
+/**
+ * @brief Unmarshal machine ok event from netlink message
+ */
+int dmc_netlink_unmarshal_event_machine_ok(
+    struct dmc_event_machine_ok  *event,
+    struct dmc_netlink_event_msg *event_msg);
+
+/**
+ * @brief Unmarshal debug event from netlink message
+ */
+int dmc_netlink_unmarshal_event_debug(struct dmc_event_debug       *event,
+                                      struct dmc_netlink_event_msg *event_msg);
+
 /* ################################# */
 /*     Functions for marshalling     */
 /*  from netlink messages to events  */
