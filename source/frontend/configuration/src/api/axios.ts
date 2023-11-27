396767f871630a551691c1cff912c3c46660444b
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-11-27 11:56:04 +0100

    refactor: prep for production build

--- source/frontend/configuration/src/api/axios.ts
+++ source/frontend/configuration/src/api/axios.ts
@@ -1,12 +1,16 @@
 /** @format */
-
-import ENV from "@/constants/env";
 import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+import ENV from "@/constants/env";
 
-export const http = axios.create({
-  baseURL: ENV.API_URL || "http://localhost:9000",
-  timeout: 20000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+console.log(ENV);
+
+export const mockInstance = axios.create();
+
+export const mock = new AxiosMockAdapter(mockInstance, {
+  delayResponse: 200,
 });
+
+const realInstance = axios.create();
+
+export default ENV.MOCKED ? mockInstance : realInstance;
