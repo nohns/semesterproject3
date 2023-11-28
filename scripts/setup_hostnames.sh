bfd6639e411230f4379b61bfc6bc56003672b407
Asger Nohns <asgernohns@gmail.com>
2023-11-28 10:44:26 +0000

    fix(scripts): correct unit name + daemon-reload

--- scripts/setup_hostnames.sh
+++ scripts/setup_hostnames.sh
@@ -10,11 +10,12 @@ chmod +x /home/pi/scripts/publish_mdns_cnames.py
 chown pi:pi /home/pi/scripts/publish_mdns_cnames.py
 
 echo "Installing hostname publish service to /etc/systemd/system/..."
-cp resources/publish_mdns_cnames.service /etc/systemd/system/
-chmod 755 /etc/systemd/system/publish_mdns_cnames.service
-chown root:root /etc/systemd/system/publish_mdns_cnames.service
+cp resources/avahi-mdns-cname.service /etc/systemd/system/
+chmod 755 /etc/systemd/system/avahi-mdns-cname.service
+chown root:root /etc/systemd/system/avahi-mdns-cname.service
 
 echo "Enabling and starting hostname publish service..."
-systemctl enable publish_mdns_cnames.service
-systemctl start mdns-publisher.service
+systemctl daemon-reload
+systemctl enable avahi-mdns-cname.service
+systemctl start avahi-mdns-cname.service
 
