7dc15fa2c7efd99b7ee717d9ca7cae00644a7eb4
Asger Nohns <asgernohns@gmail.com>
2023-11-28 11:38:29 +0000

    fix(scripts): use correct bash eq operators

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-    echo "ERROR: No build directory specified! use 'touch' or 'configuration'"
+    echo "ERROR: No build directory specified! use 'touchscreen' or 'configuration'"
     exit 1
 fi
 
 # Check that first argument is either 'touch' or 'configuration' 
 if [ "$1" != "touchscreen" ] && [ "$1" != "configuration" ]; then
-    echo "ERROR: First argument must be either 'touch' or 'configuration'"
+    echo "ERROR: First argument must be either 'touchscreen' or 'configuration'"
     exit 1
 fi
 
 # If 'touch' then define VHOST_DIR as 'touch.pi.local' and 'configuration' as 'config.pi.local'
-if [ "$1" == "touchscreen" ]; then
+if [ "$1" = "touchscreen" ]; then
     VHOST_DIR="touch.pi.local"
-elif [ "$1" == "configuration" ]; then
+elif [ "$1" = "configuration" ]; then
     VHOST_DIR="config.pi.local"
 fi
 
